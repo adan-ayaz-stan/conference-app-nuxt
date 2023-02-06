54f3fe624b9451793ae7f92828fb20c915703516
Adan <999a.d.a.n.a.y.a.z999@gmail.com>
2023-02-06 16:51:08 +0500

    Update UI

--- components/UI/Button.vue
+++ components/UI/Button.vue
@@ -1,13 +1,15 @@
 <template>
-	<NuxtLink
-		to="/signup"
-		:class="`px-4 py-1 transition-all md:px-5 lg:px-6 xl:px-8 hover:cursor-pointer hover:opacity-75 ${props.bg}`">
-		<slot
-	/></NuxtLink>
+  <NuxtLink
+    to="/signup"
+    :class="`px-4 py-1 transition-all md:px-5 lg:px-6 xl:px-8 hover:cursor-pointer hover:opacity-75 ${props.bg}`"
+  >
+    <slot
+  /></NuxtLink>
+  <!-- Restore background -->
 </template>
 
 <script setup lang="ts">
-	const props = defineProps<{
-		bg?: string;
-	}>();
+const props = defineProps<{
+  bg?: string;
+}>();
 </script>
